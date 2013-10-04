ae8022d52c305bf28b2f13d6d22fbb91c5297212
Chris Lee <sihrc.c.lee@gmail.com>
2013-10-03 21:55:26 -0400

    Initialized Main.c need to catch up with powertrain on motor controller designs

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ *REVO Sensing - Motor Controller 
+ *written by: Chris Lee (@sihrc)
+ *9/27/2013
+
+ */
+#define F_CPU 1000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int main(){
+    
+}
