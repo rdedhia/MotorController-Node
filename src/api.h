c8e8e0ea4df9a1cb335776687789d69446c17f21
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-04-23 19:43:32 -0400

    Added the correct api

--- src/api.h
+++ src/api.h
@@ -29,7 +29,6 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-
 /* Definitions for node IDs and message IDs.
  * Please feel free to add IDs as necessary, but make sure
  *  to synchronize this file between all nodes
@@ -37,19 +36,26 @@
  * NODE: max of 5 bits
  * MSG: max of 6 bits
  */
-#define NODE_watchdog       0
-#define NODE_bms            1
-#define NODE_speedometer    2
-#define NODE_halleffect     3
-#define NODE_sdlogger       4
+#define NODE_watchdog       ((uint8_t) 0x0)
+#define NODE_bms            ((uint8_t) 0x1)
+#define NODE_broadcast      ((uint8_t) 0x2) // All nodes listen for a broadcast
+#define NODE_speedometer    ((uint8_t) 0x3)
+#define NODE_halleffect     ((uint8_t) 0x4)
+#define NODE_sdlogger       ((uint8_t) 0x5)
+#define NODE_demoClient     ((uint8_t) 0xA)
+#define NODE_demoServer     ((uint8_t) 0xB)
+#define NODE_demoNode1      ((uint8_t) 0xC)
+#define NODE_demoNode2      ((uint8_t) 0xD)
+#define NODE_demoNode3      ((uint8_t) 0xE)
 
-#define MSG_critical        0
-#define MSG_warning         1
-#define MSG_speed           2
-#define MSG_voltagelow		3
-#define MSG_shunting		4
-#define MSG_shutdown        5
-#define MSG_data_other      6
+#define MSG_critical        ((uint8_t) 0x0)
+#define MSG_warning         ((uint8_t) 0x1)
+#define MSG_speed           ((uint8_t) 0x2)
+#define MSG_voltagelow		((uint8_t) 0x3)
+#define MSG_shunting		((uint8_t) 0x4)
+#define MSG_shutdown        ((uint8_t) 0x5)
+#define MSG_data_other      ((uint8_t) 0x6)
+#define MSG_demoMsg         ((uint8_t) 0xA)
 
 //#define MSG_batteryLevel    3;
 //#define MSG_batteryUsage    4;
@@ -65,13 +71,13 @@ int initCAN(uint8_t nodeID);
 /* implemented in api.c
  * Sample call: sendCANmsg(NODE_watchdog,MSG_critical,data,dataLen);
  */
-int sendCANmsg(uint8_t destID, uint8_t msgID, char* msg, uint8_t msgLen);
+int sendCANmsg(uint8_t destID, uint8_t msgID, uint8_t* msg, uint8_t msgLen);
 
 /* must be implemented by user. A sample implementation is included in
  *  a block comment in api.c
  * make sure not to do any heavy computation in this method, since it
  *  will be called from an ISR and will delay your main loop
  */
-void handleCANmsg(uint8_t destID, uint8_t msgID, char* msg, uint8_t msgLen);
+void handleCANmsg(uint8_t destID, uint8_t msgID, uint8_t msg[], uint8_t msgLen);
 
 #endif
