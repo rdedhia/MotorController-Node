dcbe869d9b06beed366b1a0715c8ca39a5ca6715
rdedhia <dedhiar.12@gmail.com>
2014-05-03 18:03:57 -0400

    fixed timer stuff hopefully for PWM

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
  */
 #include "api.h"
+#include "uart.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <inttypes.h>
@@ -12,9 +13,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define F_CPU (1000000L)
+#define TIMESCALAR 1024.0
+#define F_IO 1000000
+#define DT (TIMESCALAR / F_IO) //real time to atmega time conversion
+#define PWM_FREQ 20000
+#define CYCLE (1 / PWM_FREQ) //switching time
+#define INTERNAL (CYCLE / DT)
+
 uint8_t SHUTDOWN = 0;
 
 int main(){
+    //Initializing Uart
+    initUART();
     //Setting Pins
     //Totem Control Pins R-PD4 (pin 1/OC1A), L-PC1 (pin 2/OC1B)
     DDRD |= _BV(PD4)|_BV(PC1); //To Output
@@ -25,22 +36,31 @@ int main(){
 
     DDRB &= ~_BV(PB2); // Input Throttle
 
-    //Setting up all the other things before looping
+    // //Setting up all the other things before looping
+    // //Enable ADC, set prescalar to 128 (slow down ADC clock)
+    // ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+    // //Enable internal reference voltage
+    // ADCSRB &= _BV(AREFEN);
+    // //Set internal reference voltage as AVcc
+    // ADMUX |= _BV(REFS0);
+    // // Left adjust ADC result to allow easy 8 bit reading 
+    // ADMUX |= (1 << ADLAR);
+
+    // // Set ADC to Free-Running Mode 
+    // ADCSRA &= ~(1 << ADTS0)&~(1 << ADTS1)&~(1 << ADTS2)&~(1 << ADTS3);  
+    // // Enable ADC 
+    // ADCSRA |= (1 << ADEN);
+    // // Start A2D Conversions 
+    // ADCSRA |= (1 << ADSC);
+
     //Enable ADC, set prescalar to 128 (slow down ADC clock)
     ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
     //Enable internal reference voltage
     ADCSRB &= _BV(AREFEN);
     //Set internal reference voltage as AVcc
     ADMUX |= _BV(REFS0);
-    // Left adjust ADC result to allow easy 8 bit reading 
-    ADMUX |= (1 << ADLAR);
-
-    // Set ADC to Free-Running Mode 
-    ADCSRA &= ~(1 << ADTS0)&~(1 << ADTS1)&~(1 << ADTS2)&~(1 << ADTS3);  
-    // Enable ADC 
-    ADCSRA |= (1 << ADEN);  
-    // Start A2D Conversions 
-    ADCSRA |= (1 << ADSC);  
+    //Set ADMUX to ADC5 (0x06)
+    ADMUX |= 0x06;
 
     //Enter Loop
     uint8_t ch; //Selects ADC Channel
@@ -49,13 +69,9 @@ int main(){
     //For PWM
     // enable timer0 for fast pwm with prescaler of one. 
    TCCR0A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1)|(1<<CS00); 
-   TCCR1A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1)|(1<<CS00); 
+   TCCR1A|=(1<<WGM00)|(1<<WGM01)|(1<<COM0A1)|(1<<CS00);
 
     for (;;) {
-        //Select channel 0 for analog input for reading the pot
-        ch = 0;
-         //Have the lowest 4 bits of ADMUX select the channel we want to read
-        ADMUX = (0<<REFS0) | PB2;
         //Wait for ADC reading
         ADCSRA |=  _BV(ADSC);
         while(bit_is_set(ADCSRA, ADSC));
@@ -63,25 +79,42 @@ int main(){
         //Get the value at PB4 (button input)
         int val = PINB & _BV(PB4);
 
-        //Getting the percentage voltage of potentiometer
-        percentage = ADC*5.0;
+        //Percentage is fraction equal to duty cycle
+        //If Vin = 2.5V, percentage = 0.5
+        percentage = ADC/1023;
+        //ADC = (Vin / 5.0) * 1023
+
+        // _delay_ms(500);
+        int size = sizeof(double)/sizeof(char);
+        char str[size];
+        sprintf(str, "%f", percentage);
+        UART_putString(str);
+        UART_putString("\nHello World\n");
         
         //Setting the value at PB6 to the LED at PB5
         //Based on value, forward or backward
         if (SHUTDOWN == 0) { //emergency shut off
         if (val == 0){ //backward
             PORTB &= ~_BV(PB3); //Setting LED to Low
-
             PORTB &= ~_BV(PD4);  //Setting Left to Low
-
-            TCNT1 = 0; //Reset the other timer not for the right motor
-            OCR0A = percentage/1024; //PWM input to Right
+            if (TCNT1 <= percentage*INTERNAL) {
+                PORTB |= _BV(PC1);
+            } else if (TCNT1 < INTERNAL) {
+                PORTB &= ~_BV(PC1);
+            } else {
+                TCNT1 = 0;
+            }
         }
         else{ //forward
             PORTB |= _BV(PB3); //Setting LED to High
             PORTB &= ~_BV(PC1); //Setting Right to Low
-            TCNT0 = 0; //Reset the other timer not for the left motor
-            OCR1A = percentage/1024; //PWM input to Left
+            if (TCNT1 <= percentage*INTERNAL) {
+                PORTB |= _BV(PD4);
+            } else if (TCNT1 < INTERNAL) {
+                PORTB &= ~_BV(PD4);
+            } else {
+                TCNT1 = 0;
+            }
         }
     }
     }
